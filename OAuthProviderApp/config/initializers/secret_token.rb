b901c0b32d9082edff1b2a496e21c210e688737f
adamjreilly <adam.reilly@logik.com>
2014-04-21 13:11:40 +0000

    Generate provider app

---
+++ OAuthProviderApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OAuthProviderApp::Application.config.secret_token = '6f4ef5ec4b73168818b65c17675f532a564fddce96e63acb44741c8716b1269b12ac623794ef8614351f51c37812ad28b5078cbde5e50734ba8efb17dc9076c6'
