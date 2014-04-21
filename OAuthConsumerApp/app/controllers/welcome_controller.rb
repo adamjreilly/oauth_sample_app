4b5cf3d9fae66ac15d913733cc9f573446c0ba0d
adamjreilly <adam.reilly@logik.com>
2014-04-21 15:07:59 +0000

    Welcome controller logic

--- OAuthConsumerApp/app/controllers/welcome_controller.rb
+++ OAuthConsumerApp/app/controllers/welcome_controller.rb
@@ -1,4 +1,16 @@
 class WelcomeController < ApplicationController
   def index
+    if(current_user)
+      @consumer_tokens = TestToken.all :conditions => {:user_id => current_user.id}
+
+      if @consumer_tokens.first
+        @token = @consumer_tokens.first.client
+        render inline @token.get("/api/v1/show").body
+      else
+        redirect_to '/oauth_consumers/test'
+      end
+    else
+      redirect_to new_user_registration_path
+    end
   end
 end
