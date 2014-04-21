c4f86141fd2aeea0c52507b44055dafa7f46ff27
adamjreilly <adam.reilly@logik.com>
2014-04-21 14:18:29 +0000

    Generate new rails app for consumer

---
+++ OAuthConsumerApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OAuthConsumerApp::Application.config.secret_token = '2b26df8e536feb8bacc4f7f387e8f43facb619db43d75608ba3de90ae7d01d2789f2078caebb48c31395cc359b4bbaed6f62265d7108f7249ff2480af2910b9c'
