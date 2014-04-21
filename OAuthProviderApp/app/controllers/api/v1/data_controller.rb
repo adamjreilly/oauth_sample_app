4066323aa6c62d17f98aece83f33ff39ff2ed953
adamjreilly <adam.reilly@logik.com>
2014-04-21 13:52:43 +0000

    add fake protected data

--- OAuthProviderApp/app/controllers/api/v1/data_controller.rb
+++ OAuthProviderApp/app/controllers/api/v1/data_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::DataController < ApplicationController
+  respond_to :json, :xml
+
+  oauthenticate :interactive => false
+
+  def show
+    respond_with 'This is sensitive information'
+  end
 end
